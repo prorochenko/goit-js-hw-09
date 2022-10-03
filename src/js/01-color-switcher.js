7b223368ceaa3b3f1e2f3987a93215808b2e205e
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-10-03 21:25:42 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector(`[data-start]`),
+  stopBtn: document.querySelector(`[data-stop]`),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener(`click`, startChangeColor);
+refs.stopBtn.addEventListener(`click`, stopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startChangeColor(evt) {
+  timerId = setInterval(changeBodyBgColor, 1000);
+  evt.currentTarget.disabled = true;
+}
+
+function changeBodyBgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
